c6eab136dc4a50ef974a75480bc69cf8db98089c
Vidula <olia.sivor@gmail.com>
2019-04-02 22:45:33 +0300

    timeline

--- src/main/java/ru/sivoronova/App.java
+++ src/main/java/ru/sivoronova/App.java
@@ -1,13 +1,28 @@
 package ru.sivoronova;
 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws TwitterException {
+        // The factory instance is re-useable and thread safe.
+        Twitter twitter = TwitterFactory.getSingleton();
+
+        List<Status> statuses = twitter.getUserTimeline("vishwananda");
+        System.out.println("Showing home timeline.");
+        for (Status status : statuses) {
+            System.out.println(status.getCreatedAt() + "," +
+                    status.getText());
+        }
+
     }
 }
